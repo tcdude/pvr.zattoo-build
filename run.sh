fa716c47c3f0c6dda13ca657ba974d8b0830ebc6
tcdude <tc@tizilogic.com>
2026-03-16 02:49:40 +0100

    fingers crossed...

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml stop build-pvr.zattoo_x64
-docker-compose -f docker-compose.yml rm build-pvr.zattoo_x64
-docker-compose -f docker-compose.yml pull build-pvr.zattoo_x64
-docker-compose -f docker-compose.yml build build-pvr.zattoo_x64
-docker-compose -f docker-compose.yml run --remove-orphans build-pvr.zattoo_x64
+docker compose -f docker-compose.yml stop build-pvr.zattoo_x64
+docker compose -f docker-compose.yml rm build-pvr.zattoo_x64
+docker compose -f docker-compose.yml pull build-pvr.zattoo_x64
+docker compose -f docker-compose.yml build build-pvr.zattoo_x64
+docker compose -f docker-compose.yml run --remove-orphans build-pvr.zattoo_x64
 
-docker-compose -f docker-compose.yml stop build-pvr.zattoo_aarch64
-docker-compose -f docker-compose.yml rm build-pvr.zattoo_aarch64
-docker-compose -f docker-compose.yml pull build-pvr.zattoo_aarch64
-docker-compose -f docker-compose.yml build build-pvr.zattoo_aarch64
-docker-compose -f docker-compose.yml run --remove-orphans build-pvr.zattoo_aarch64
+docker compose -f docker-compose.yml stop build-pvr.zattoo_aarch64
+docker compose -f docker-compose.yml rm build-pvr.zattoo_aarch64
+docker compose -f docker-compose.yml pull build-pvr.zattoo_aarch64
+docker compose -f docker-compose.yml build build-pvr.zattoo_aarch64
+docker compose -f docker-compose.yml run --remove-orphans build-pvr.zattoo_aarch64
